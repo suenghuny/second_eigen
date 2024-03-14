4bc78c1ac45ba03ca40e36a46b4ae8885f36400a
suenghun <63846187+suenghun@users.noreply.github.com>
2024-03-14 22:09:59 +0900

    backward 수정

--- main_rev_agent_grouping.py
+++ main_rev_agent_grouping.py
@@ -20,7 +20,7 @@ if vessl_on == True:
         os.makedirs(output_dir)
 else:
     from torch.utils.tensorboard import SummaryWriter
-    output_dir = "/output/"
+    output_dir = "output/"
     import os
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
