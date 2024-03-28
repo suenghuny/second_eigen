47991f2625e4b91e5632a51220ecbe1d1bd89a33
suenghun <63846187+suenghun@users.noreply.github.com>
2024-03-28 09:24:27 +0900

    sampling - 2

--- GLCN/GLCN.py
+++ GLCN/GLCN.py
@@ -265,7 +265,7 @@ class GLCN(nn.Module):
                             Wv = H @ self.Wv[k]
                             a = self._prepare_attentional_mechanism_input(Wq, Wv, k = k)
                             zero_vec = -9e15 * torch.ones_like(A)
-                            a = torch.where(A > 0, a, zero_vec)
+                            a = torch.where(A > 0.01, a, zero_vec)
                             a = F.softmax(a, dim=1)
                             H = F.relu(torch.matmul(a, Wh))
                             if k+1 == self.k_hop:
--- main_rev_agent_grouping.py
+++ main_rev_agent_grouping.py
@@ -199,7 +199,6 @@ def main():
     hidden_size_comm = int(os.environ.get("hidden_size_comm", 84))#cfg.hidden_size_comm
     hidden_size_action = int(os.environ.get("hidden_size_action", 84))  # cfg.hidden_size_comm
     hidden_size_Q = int(os.environ.get("hidden_size_Q",128)) #cfg.hidden_size_Q         # GAT 해당
-
     n_representation_obs = int(os.environ.get("n_representation_obs", 64))#cfg.n_representation_obs  # GAT 해당
     n_representation_action = int(os.environ.get("n_representation_action", 56))  # cfg.n_representation_comm
     n_representation_comm = int(os.environ.get("n_representation_comm", 48))#cfg.n_representation_comm
