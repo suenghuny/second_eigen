e6562cd01b0b46bab9ab741b47cf89a1cd75a998
suenghun <63846187+suenghun@users.noreply.github.com>
2024-04-27 10:18:48 +0900

    load model-6

--- main_rev_agent_grouping.py
+++ main_rev_agent_grouping.py
@@ -263,7 +263,7 @@ def main():
                    env = None
                   )
     if load_model==True:
-        agent.load_model("episode61790.pt")#
+        agent.load_model("episode67190.pt")#
     t = 0
     epi_r = []
     win_rates = []
