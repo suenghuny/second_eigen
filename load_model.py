dcdb6567d173e2f8fdaa98537d3ec7aba6214123
suenghun <63846187+suenghun@users.noreply.github.com>
2024-04-30 16:11:07 +0900

    훈련-11

--- load_model.py
+++ load_model.py
@@ -332,7 +332,7 @@ def main():
             else:
                 wr_df = pd.DataFrame(win_rates)
                 wr_df.to_csv("win_rate_map_name_{}_GNN_{}_lr_{}_hiddensizeobs_{}_hiddensizeq_{}_nrepresentationobs_{}.csv".format(map_name1, learning_rate, hidden_size_obs, hidden_size_Q, n_representation_obs, n_representation_comm))
-                if win_rate >= 0.4:
+                if win_rate >= 0.5:
                     agent.save_model(output_dir, e, t, win_rate)
                     win_rate_count += 1
 
