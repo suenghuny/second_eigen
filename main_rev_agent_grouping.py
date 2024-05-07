6c77d9e486f158be1ece4b2642135e0bce076bca
suenghun <63846187+suenghun@users.noreply.github.com>
2024-05-07 10:55:13 +0900

    AHHH2

--- main_rev_agent_grouping.py
+++ main_rev_agent_grouping.py
@@ -227,7 +227,7 @@ def main():
     graph_embedding_comm = int(os.environ.get("graph_embedding_comm", 84))
     buffer_size = int(os.environ.get("buffer_size", 100000))       # cfg.buffer_size
     batch_size = int(os.environ.get("batch_size", 24))             # cfg.batch_size
-    gamma = 0.99                                                            # cfg.gamma
+    gamma = 0.99      ##                                                      # cfg.gamma
     learning_rate = float(os.environ.get("learning_rate", 5.0e-4))            # cfg.lr
     learning_rate_graph = learning_rate  # cfg.lr
     num_episode = 500000 #cfg.num_episode
